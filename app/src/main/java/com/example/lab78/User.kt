6fa8950ebc66438633b54bf6e573704bef832ece
HUYRE <168230552+HUYRE@users.noreply.github.com>
2024-09-22 23:51:03 -0500

    an

---
+++ app/src/main/java/com/example/lab78/User.kt
@@ -0,0 +1,12 @@
+package com.example.lab78
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "user_table")
+data class User(
+    @PrimaryKey(autoGenerate = true) val id: Long? = null,
+    val nombre: String,
+    val edad: Int,
+    val peso: Float,
+    val altura: Float
+)
---
+++ app/src/main/java/com/example/lab78/UserVIewModel.kt
@@ -0,0 +1,26 @@
+package com.example.lab78
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+
+class UserViewModel(private val repository: UserRepository) : ViewModel() {
+    val allUsers = repository.getAllUsers()
+
+    fun insert(user: User) {
+        viewModelScope.launch {
+            repository.insert(user)
+        }
+    }
+
+    fun update(user: User) {
+        viewModelScope.launch {
+            repository.update(user)
+        }
+    }
+
+    fun delete(user: User) {
+        viewModelScope.launch {
+            repository.delete(user)
+        }
+    }
+}
